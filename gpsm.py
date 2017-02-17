18a5f32f6f4359267bc56dbd73d1787cbcabab36
Chad Voegele <cavoegele@gmail.com>
2017-02-17 10:49:46 -0600

    Return seletivity values to host from kernel.

--- gpsm.py
+++ gpsm.py
@@ -13,8 +13,7 @@ ast = Module([
   CDeclGlobal(("int", "process_prog_arg(int argc, char *argv[], int arg_start)", "{ return 1; }")),
   CDeclGlobal(("extern int", "SKELAPP_RETVAL", "")),
   CDeclGlobal(("extern const char*", "OUTPUT", "")),
-  Kernel("selectivity", [dgraph.param(), qgraph.param(), ('int*', 'dprop_ptr'), ('int*', 'qprop_ptr')],
-      [
+  Kernel("calc_selectivity", [dgraph.param(), qgraph.param(), ('int*', 'dprop_ptr'), ('int*', 'qprop_ptr'), ('float*', 'selectivity_ptr')], [
         ForAll("node", qgraph.nodes(), [
           CDecl(("int", "degree", "= %s" % qgraph.edges("node").size())),
           CDecl(("int", "freq", "= 0")),
@@ -26,11 +25,12 @@ ast = Module([
           ]),
           CDecl(("float", "selectivity", "= 1.0*degree/freq")),
           CBlock(['printf("node: %d, degree: %d, freq: %d, selectivity: %f\\n", node, degree, freq, selectivity)']),
-          ])
-      ]
-      ),
+          CBlock(['selectivity_ptr[node] = selectivity']),
+        ])
+      ]),
   Kernel("gg_main", [params.GraphParam('hg', True), params.GraphParam('gg', True), params.GraphParam('qhg', True), params.GraphParam('qgg', True), ('Shared<int>&', 'dprop'), ('Shared<int>&', 'qprop')],
       [
-          Invoke('selectivity', ('gg', 'qgg', 'dprop.gpu_rd_ptr()', 'qprop.gpu_rd_ptr()')),
+          CDecl(('Shared<float>', 'selectivity', '= qhg.nnodes')),
+          Invoke('calc_selectivity', ('gg', 'qgg', 'dprop.gpu_rd_ptr()', 'qprop.gpu_rd_ptr()', 'selectivity.gpu_wr_ptr()')),
       ])
 ])
