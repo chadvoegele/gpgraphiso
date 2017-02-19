f2de448b66b34bd8bff965a852560c27233a2b84
Chad Voegele <cavoegele@gmail.com>
2017-02-19 12:52:21 -0600

    Add environment variable for base directory.

--- env/chad.sh
+++ env/chad.sh
@@ -1,5 +1,6 @@
 BASE=$(readlink -f $(dirname $0))/..
 export MAKEJOBS=6
 export GGC_ROOT="$BASE/ggc"
+export GPGRAPHISO_ROOT="$BASE"
 export GPGRAPHLIB_ROOT="$BASE/gpgraphlib"
 export PYTHONPATH="$BASE/gpsm:$GGC_ROOT/src:$BASE"
