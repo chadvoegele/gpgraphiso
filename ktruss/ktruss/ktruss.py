b53b0e1bed298d6736fd87964cceb8578f1f548b
Chad Voegele <cavoegele@gmail.com>
2017-05-01 18:58:17 -0500

    Start NSA ktruss algo.

--- ktruss/ktruss/ktruss.py
+++ ktruss/ktruss/ktruss.py
@@ -4,309 +4,51 @@ import gg.lib.aol
 from gg.types import RangeIterator
 
 WL = gg.lib.wl.Worklist()
-max_ktruss_nodes = gg.lib.aol.AppendOnlyList('max_ktruss_nodes')
 graph = gg.lib.graph.Graph('graph')
 
-def EL(node):
-    return LaunchConfig(node, blocks = "edge_blocks")
-
-def NL(node):
-    return LaunchConfig(node, blocks = "node_blocks")
-
 ast = Module([
-    CBlock([cgen.Include("kernels/segmentedsort.cuh")]),
-    CBlock([cgen.Include("kernels/reducebykey.cuh")]),
     CBlock([cgen.Include("ktruss.h")]),
     CDecl(('unsigned', 'KTRUSS_K', '= 4')),
     CDecl(('int', 'CUDA_DEVICE', '= 0')),
     CDecl(('mgpu::ContextPtr', 'mgc', '')),
-    Kernel("preprocess", [graph.param(), ('unsigned int *', 'valid_edges')], [
-        CDecl(('const index_type', 'last', ' = graph.nnodes')),
-        ForAll("node", graph.nodes(), [
-            CDecl(('bool', 'pop', '')),
-            CDecl(('int', 'degree', '')),
-            Assign('pop', graph.valid_node("node")),
-            If("pop", [
-                CBlock('degree = graph.getOutDegree(node)')
-            ]),
-            ClosureHint(ForAll("edge", graph.edges("node"), [
-                CDecl(('index_type', 'dst', '= graph.getAbsDestination(edge)')),
-                CDecl(('int', 'dst_degree', '= graph.getOutDegree(dst)')),
-                If('(dst_degree > degree) || (dst_degree == degree && dst > node)', [
-                    CBlock(["graph.edge_data[edge] = dst"]),
-                    CBlock(["atomicAdd(valid_edges + node, 1)"]),
-                ], [
-                    # + dst, makes resulting edge_dst = [ sorted valid, sorted invalid ]
-                    CBlock(["graph.edge_data[edge] = graph.nnodes + dst" ]),
-                ]),
-            ])),
-        ])
-    ]),
-    Kernel("intersect", [graph.param(), ('index_type', 'u'), ('index_type', 'v'), ('unsigned int *', 'valid_edges'), ('int *', 'count'), ('index_type', 'edge')], [
-        CDecl(('index_type', 'u_start', '= graph.getFirstEdge(u)')),
-        CDecl(('index_type', 'u_end', '= u_start + valid_edges[u]')),
-        CDecl(('index_type', 'v_start', '= graph.getFirstEdge(v)')),
-        CDecl(('index_type', 'v_end', '= v_start + valid_edges[v]')),
-        CDecl(('index_type', 'u_it', '= u_start')),
-        CDecl(('index_type', 'v_it', '= v_start')),
-        CDecl(('index_type', 'a', '')),
-        CDecl(('index_type', 'b', '')),
-        While('u_it < u_end && v_it < v_end', [
-            CBlock('a = graph.getAbsDestination(u_it)'),
-            CBlock('b = graph.getAbsDestination(v_it)'),
-            CDecl(('int', 'd', '= a - b')),
-            If('d == 0', [
-                CBlock('atomicAdd(count + u_it, 1)'),
-                CBlock('atomicAdd(count + v_it, 1)'),
-                CBlock('atomicAdd(count + edge, 1)'),
-            ]),
-            If('d <= 0', [CBlock('u_it++')]),
-            If('d >= 0', [CBlock('v_it++')]),
+    Kernel('init_degree', [graph.param(), ('unsigned*', 'degrees'), ('unsigned', 'k')], [
+      ForAll("node", graph.nodes(), [
+        CDecl(('int', 'degree', '= graph.getOutDegree(node)')),
+        CBlock('degrees[node] = degree'),
+        If('degrees[node] < k - 1', [
+          WL.push("node"),
         ]),
-    ], device=True),
-    Kernel("count_triangles", [graph.param(), ('unsigned int *', 'valid_edges'), ('int *', 'count')], [
-        CDecl(('int', 'lcount', '=0')),
-        ForAll("v", graph.nodes(), [
-            CDecl(('bool', 'pop', '')),
-            Assign('pop', graph.valid_node("v")),
-            ClosureHint(ForAll("edge", graph.edges("v", limit='valid_edges[v]'), [
-                CDecl(('index_type', 'u', '= graph.getAbsDestination(edge)')),
-                CDecl(('index_type', 'd_u', '= graph.getOutDegree(u)')),
-                CBlock('intersect(graph, u, v, valid_edges, count, edge)'),
-            ])),
+      ]),
+    ]),
+    Kernel('degree_filter_iter', [graph.param(), ('unsigned*', 'degrees'), ('unsigned', 'k')], [
+      ForAll('wli', WL.items(), [
+        CDecl(('int', 'src', '')),
+        CDecl(('bool', 'pop', '')),
+        WL.pop('pop', 'wli', 'src'),
+        For('edge', graph.edges('src'), [
+          CDecl(('int', 'dst', '= graph.getAbsDestination(edge)')),
+          CDecl(('int', 'nnodes', '= graph.nnodes')),
+          CBlock('atomicDec(&degrees[dst], nnodes)'),
+          If('degrees[dst] < k - 1', [
+            WL.push('dst'),
+          ]),
+          CBlock('atomicExch(&degrees[src], nnodes)'),
         ]),
+      ]),
     ]),
-    Kernel("count_triangle_back_edge", [graph.param(), ('unsigned int *', 'valid_edges'), ('int *', 'count')], [
-        ForAll('src', graph.nodes(), [
-            ClosureHint(ForAll("edge", graph.edges('src', limit='valid_edges[src]'), [
-                CDecl(('index_type', 'dst', '= graph.getAbsDestination(edge)')),
-                CDecl(('index_type', 'back_idx', '= findBackEdge(graph, src, dst, valid_edges)')),
-                CBlock('count[back_idx] = count[edge]'),
-            ])),
+    Kernel('degree_filter', [params.GraphParam('g', True), params.GraphParam('gg', True)], [
+      CDecl(('dim3', 'blocks', '')),
+      CDecl(('dim3', 'threads', '')),
+      CBlock(['kernel_sizing(g, blocks, threads)']),
+      CDecl(('Shared<unsigned>', 'degrees', '(g.nnodes)')),
+      Pipe([
+        Invoke('init_degree', ['gg', 'degrees.gpu_wr_ptr()', 'KTRUSS_K']),
+        Pipe([
+          Invoke('degree_filter_iter', ['gg', 'degrees.gpu_wr_ptr()', 'KTRUSS_K']),
         ]),
-    ]),
-    Kernel('count_triangle_edges', [('CSRGraphTy&', 'g'), ('CSRGraphTy&', 'gg'), ('Shared<int>&', 'count')], [
-        CDecl(('dim3', 'blocks', '')),
-        CDecl(('dim3', 'threads', '')),
-        CBlock(['kernel_sizing(g, blocks, threads)']),
-        CBlock("count.zero_gpu()"),
-        CDecl(('Shared<unsigned int>', 'valid_edges', '(g.nnodes)')),
-        CBlock("valid_edges.zero_gpu()"),
-        Invoke("preprocess", ['gg', 'valid_edges.gpu_wr_ptr()']),
-        CBlock("mgpu::SegSortPairsFromIndices(gg.edge_data, gg.edge_dst, gg.nedges, (const int *) gg.row_start + 1, gg.nnodes - 1, *mgc);", parse=False),
-        Invoke("count_triangles", ['gg', 'valid_edges.gpu_rd_ptr()', 'count.gpu_wr_ptr()']),
-        Invoke('count_triangle_back_edge', ['gg', 'valid_edges.gpu_rd_ptr()', 'count.gpu_wr_ptr()']),
-    ], host=True),
-    Kernel("init", [graph.param()], [
-        ForAll("node", graph.nodes(), [CBlock("graph.node_data[node] = node")])
-    ]),
-    # no equivalent since this preps the edge list
-    Kernel("prep_edge_src", [graph.param(), ('index_type *', 'edge_src')], [
-        ForAll("node", graph.nodes(), [
-            CDecl(('bool', 'pop', '')),
-            Assign('pop', graph.valid_node("node")),
-            ClosureHint(ForAll("edge", graph.edges("node"), [
-                CBlock("edge_src[edge] = node")
-            ]))
-        ])
-    ]),
-    # _Select_winner_init
-    Kernel("hook_init", [graph.param(), ('index_type *', 'edge_src'), ('int*', 'edge_tri_count'), ('unsigned', 'KTRUSS_K')], [
-        ForAll("edge", RangeIterator("graph.nedges"), [
-            If('edge_tri_count[edge] >= KTRUSS_K-2', [
-                CDecl(('index_type', 'x', '= edge_src[edge]')),
-                CDecl(('index_type', 'y', '= graph.getAbsDestination(edge)')),
-                CDecl(('index_type', 'mx', '= x > y ? x : y')),
-                CDecl(('index_type', 'mn', '= x > y ? y : x')),
-                CBlock("graph.node_data[mx] = mn"),
-            ]),
-        ]),
-    ]),
-    # select_winner2
-    Kernel("hook_high_to_low", [graph.param(), ('const __restrict__ index_type *', 'edge_src'), ('char *', 'marks'), ('int*', 'edge_tri_count'), ('unsigned', 'KTRUSS_K')], [
-        ForAll("edge", RangeIterator("graph.nedges"), [
-            If('edge_tri_count[edge] >= KTRUSS_K-2', [
-                If("!marks[edge]", [
-                    CDecl(('index_type', 'u', '= edge_src[edge]')),
-                    CDecl(('index_type', 'v', '= graph.getAbsDestination(edge)')),
-                    CDecl(('node_data_type', 'p_u', '= graph.node_data[u]')),
-                    CDecl(('node_data_type', 'p_v', '= graph.node_data[v]')),
-                    CDecl(('index_type', 'mx', '= p_u > p_v ? p_u : p_v')),
-                    CDecl(('index_type', 'mn', '= p_u > p_v ? p_v : p_u')),
-                    If("mx == mn", [
-                        CBlock("marks[edge] = 1")
-                    ], [
-                        CBlock("graph.node_data[mn] = mx"),
-                        ReturnFromParallelFor("true")
-                    ]),
-                ]),
-            ]),
-        ])
-    ]),
-    # select_winner
-    Kernel("hook_low_to_high", [graph.param(), ('index_type *', 'edge_src'), ('char *', 'marks'), ('int*', 'edge_tri_count'), ('unsigned', 'KTRUSS_K')], [
-        ForAll("edge", RangeIterator("graph.nedges"), [
-            If('edge_tri_count[edge] >= KTRUSS_K-2', [
-                If("!marks[edge]", [
-                    CDecl(('index_type', 'u', '= edge_src[edge]')),
-                    CDecl(('index_type', 'v', '= graph.getAbsDestination(edge)')),
-                    CDecl(('node_data_type', 'p_u', '= graph.node_data[u]')),
-                    CDecl(('node_data_type', 'p_v', '= graph.node_data[v]')),
-                    CDecl(('index_type', 'mx', '= p_u > p_v ? p_u : p_v')),
-                    CDecl(('index_type', 'mn', '= p_u > p_v ? p_v : p_u')),
-                    If("mx == mn", [
-                        CBlock("marks[edge] = 1")
-                    ], [
-                        CBlock("graph.node_data[mx] = mn"),
-                        ReturnFromParallelFor("true")
-                    ]),
-                ]),
-            ]),
-        ])
-    ]),
-    # p_jump
-    Kernel("p_jump", [graph.param()], [
-        ForAll("node", graph.nodes(), [
-            CDecl(('node_data_type', 'p_u', '= graph.node_data[node]')),
-            CDecl(('node_data_type', 'p_v', '= graph.node_data[p_u]')),
-            If("p_u != p_v", [
-                CBlock("graph.node_data[node] = p_v"),
-                ReturnFromParallelFor("true"),
-            ])
-        ])
-    ]),
-    # update_mask, note we use worklists, not masks
-    Kernel("identify_roots", [graph.param()], [
-        ForAll("node", graph.nodes(), [
-            If("graph.node_data[node] == node", [
-                WL.push("node")
-            ]),
-        ])
-    ]),
-    # p_jump_masked, but using a worklist
-    Kernel("p_jump_roots", [graph.param()], [
-        ForAll("wlnode", WL.items(), [
-            CDecl(('bool', 'pop', '')),
-            CDecl(('int', 'node', '')),
-            WL.pop("pop", "wlnode", "node"),
-            CDecl(('node_data_type', 'p_u', '= graph.node_data[node]')),
-            CDecl(('node_data_type', 'p_v', '= graph.node_data[p_u]')),
-            If("p_u != p_v", [
-                CBlock("graph.node_data[node] = p_v"),
-                ReturnFromParallelFor("true"),
-            ])
-        ])
-    ]),
-    # p_jump_unmasked, using a worklist
-    Kernel("p_jump_leaves", [graph.param()], [
-        ForAll("node", graph.nodes(), [
-            CDecl(('node_data_type', 'p_u', '= graph.node_data[node]')),
-            CDecl(('node_data_type', 'p_v', '= graph.node_data[p_u]')),
-            If("p_u != p_v", [
-                CBlock("graph.node_data[node] = p_v")
-            ])
-        ])
-    ]),
-    # same, but this is counted in timing, maybe use ReturnSum
-    Kernel("count_components", [graph.param(), ('int *', 'count')], [
-        ForAll("node", graph.nodes(), [
-            If("node == graph.node_data[node]", [
-                CBlock("atomicAdd(count, 1)")
-            ])
-        ]),
-    ]),
-    Kernel('set_gpu_ones', [('unsigned*', 'data'), ('unsigned', 'size')], [
-        ForAll('i', RangeIterator('size', ty='unsigned'), [
-            CBlock('data[i] = 1'),
-        ]),
-    ]),
-    Kernel('set_gpu_ascending', [('unsigned*', 'data'), ('unsigned', 'size')], [
-        ForAll('i', RangeIterator('size', ty='unsigned'), [
-            CBlock('data[i] = i'),
-        ]),
-    ]),
-    Kernel('set_ktruss_nodes', [graph.param(), max_ktruss_nodes.param(), ('unsigned*', 'indices'), ('int*', 'components'), ('unsigned*', 'max_component_head_index')], [
-        ForAll("node", graph.nodes(), [
-            If('graph.node_data[node] == components[*max_component_head_index]', [
-                max_ktruss_nodes.push('indices[node]'),
-            ])
-        ]),
-    ]),
-    Kernel('zero_if_less_than', [('unsigned*', 'data'), ('unsigned', 'size'), ('unsigned', 'k')], [
-        ForAll('i', RangeIterator('size', ty='unsigned'), [
-            If('data[i] < k', [
-                CBlock(['data[i] = 0']),
-            ]),
-        ]),
-    ]),
-    Kernel('maximal_ktruss', [('CSRGraphTy&', 'g'), ('CSRGraphTy&', 'gg'), ('Shared<int>&', 'edge_tri_count'), ('int&', 'max_ktruss_size'), max_ktruss_nodes.param()], [
-        CDecl(('dim3', 'blocks', '')),
-        CDecl(('dim3', 'threads', '')),
-        CBlock(['kernel_sizing(g, blocks, threads)']),
-        Names(['edge_data_type']),
-        CDecl([('int', 'it_hk', '= 1'),]),
-        CDecl([('Shared<index_type>', 'edge_src', '(gg.nedges)')]),
-        CDecl([('Shared<char>', 'edge_marks', '(gg.nedges)')]),
-        CDecl(('bool', 'flag', '= false')),
-        CDecl(('int', 'edge_blocks', '')),
-        CDecl(('int', 'node_blocks', '')),
-        CBlock("edge_blocks = g.nedges / TB_SIZE + 1"),
-        CBlock("node_blocks = g.nnodes / TB_SIZE + 1"),
-        CBlock('edge_marks.zero_gpu()'),
-        NL(Invoke("prep_edge_src", ["gg", "edge_src.gpu_wr_ptr()"])),
-        NL(Invoke("init", ["gg"])),
-        EL(Invoke("hook_init", ["gg", "edge_src.gpu_rd_ptr()", 'edge_tri_count.gpu_rd_ptr()', 'KTRUSS_K'])),
-        NL(ClosureHint(Iterate("while", "any", "p_jump", ["gg"]))),
-        Pipe([DoWhile("flag", [
-            Invoke("identify_roots", ["gg"]),
-            If("it_hk != 0", [
-                EL(Invoke("hook_low_to_high", ["gg", "edge_src.gpu_rd_ptr()", "edge_marks.gpu_wr_ptr()", 'edge_tri_count.gpu_rd_ptr()', 'KTRUSS_K'])),
-                CBlock("flag = *(retval.cpu_rd_ptr())", parse=False, _scope="cpu"),
-                CBlock("flag = _rv.local", parse=False, _scope="gpu"),
-                CBlock("it_hk = (it_hk + 1) % 4")
-            ], [
-                EL(Invoke("hook_high_to_low", ["gg", "edge_src.gpu_rd_ptr()", "edge_marks.gpu_wr_ptr()", 'edge_tri_count.gpu_rd_ptr()', 'KTRUSS_K'])),
-                CBlock("flag = *(retval.cpu_rd_ptr())", parse=False, _scope="cpu"),
-                CBlock("flag = _rv.local", parse=False, _scope="gpu"),
-            ]),
-            If("!flag", [CBlock("break")]),
-            ClosureHint(Iterate("while", "any", "p_jump_roots", ["gg"])),
-            NL(Invoke("p_jump_leaves", ["gg"])),
-        ])], wlinit=WLInit("gg.nnodes", []), once=True),
-        #CBlock('printf("iterations: %d\\n", it_hk)'),
-        CDecl(('Shared<int>', 'ncomponents', '(1)')),
-        CBlock("*(ncomponents.cpu_wr_ptr()) = 0"),
-        Invoke("count_components", ['gg', 'ncomponents.gpu_wr_ptr()']),
-        CDecl(('Shared<unsigned>', 'ones', '(g.nnodes)')),
-        Invoke('set_gpu_ones', ['ones.gpu_wr_ptr()', 'g.nnodes']),
-        CDecl(('Shared<unsigned>', 'indices', '(g.nnodes)')),
-        Invoke('set_gpu_ascending', ['indices.gpu_wr_ptr()', 'g.nnodes']),
-        CDecl(('Shared<int>', 'components', '(*(ncomponents.cpu_rd_ptr()))')),
-        CDecl(('Shared<unsigned>', 'component_size', '(*(ncomponents.cpu_rd_ptr()))')),
-        CBlock('mgpu::MergesortPairs(gg.node_data, indices.gpu_wr_ptr(), g.nnodes, mgpu::less<int>(), *mgc);', parse=False),
-        CBlock('mgpu::ReduceByKey(gg.node_data, ones.gpu_wr_ptr(), g.nnodes, 0U, mgpu::plus<unsigned>(), mgpu::equal_to<int>(), components.gpu_wr_ptr(), component_size.gpu_wr_ptr(), 0, 0, *mgc);', parse=False),
-        Invoke('zero_if_less_than', ('component_size.gpu_rd_ptr()', '(*(ncomponents.cpu_rd_ptr()))', 'KTRUSS_K')),
-        CBlock('mgpu::Reduce(component_size.gpu_wr_ptr(), *(ncomponents.cpu_rd_ptr()), INT_MIN, mgpu::maximum<int>(), (int*)0, &max_ktruss_size, *mgc);', parse=False),
-        CDecl(('int', 'total_ktruss_nodes', '=0')),
-        CBlock('mgpu::Reduce(component_size.gpu_wr_ptr(), *(ncomponents.cpu_rd_ptr()), 0, mgpu::plus<int>(), (int*)0, &total_ktruss_nodes, *mgc);', parse=False),
-        CBlock('printf("total_ktruss_nodes=%d\\n", total_ktruss_nodes)'),
-        CDecl(('Shared<unsigned>', 'max_component_head_index', '(1)')),
-        CDecl(('Shared<unsigned>', 'max_component_indices', '(*(ncomponents.cpu_rd_ptr()))')),
-        Invoke('set_gpu_ascending', ['max_component_indices.gpu_wr_ptr()', '(*(ncomponents.cpu_rd_ptr()))']),
-        CBlock('mgpu::Reduce(max_component_indices.gpu_wr_ptr(), *(ncomponents.cpu_rd_ptr()), 0U, ' +
-                             'max_index<unsigned, unsigned>(component_size.gpu_rd_ptr()), ' +
-                             'max_component_head_index.gpu_wr_ptr(), (unsigned*)0, *mgc)', parse=False),
-        Invoke('set_ktruss_nodes', ['gg', 'max_ktruss_nodes', 'indices.gpu_rd_ptr()', 'components.gpu_rd_ptr()', 'max_component_head_index.gpu_rd_ptr()']),
+      ], wlinit=WLInit('4*g.nnodes'), once=True),
     ], host=True),
     Kernel("gg_main", [params.GraphParam('g', True), params.GraphParam('gg', True)], [
-        CDecl(('Shared<int>', 'count', '(g.nedges)')),
-        CBlock(['count_triangle_edges(g, gg, count)']),
-        CDecl(('int', 'max_ktruss_size', '')),
-        CDecl(('AppendOnlyList', 'max_ktruss_nodes', '(g.nnodes)')),
-        CBlock(['maximal_ktruss(g, gg, count, max_ktruss_size, max_ktruss_nodes)']),
-        CDecl(('std::vector<int>', 'ktruss_nodes_vec', '(max_ktruss_nodes.nitems())')),
-        CBlock(['ktruss_nodes_vec.assign(max_ktruss_nodes.list.cpu_rd_ptr(), max_ktruss_nodes.list.cpu_rd_ptr()+max_ktruss_nodes.nitems())']),
-        CBlock('printf("max ktruss # nodes: %d\\n", max_ktruss_size)'),
         CFor(CDecl(('unsigned', 'i', ' = 0')), 'i < ktruss_nodes_vec.size()', 'i++', [
           CBlock('printf("ktruss node[%u]=%d\\n", i, ktruss_nodes_vec[i])'),
         ]),
--- ktruss/test_ktruss/test_ktruss.py
+++ ktruss/test_ktruss/test_ktruss.py
@@ -47,13 +47,8 @@ class KTrussTests(pyirgltest.test.IrGLTest):
             CBlock([cgen.Include('gtest/gtest.h')]),
             Kernel("gg_main", [params.GraphParam('g', True), params.GraphParam('gg', True)], [
                 CBlock(['mgc = mgpu::CreateCudaDevice(CUDA_DEVICE)'], parse=False),
-                CDecl(('Shared<int>', 'count', '(g.nedges)')),
-                CBlock(['count_triangle_edges(g, gg, count)']),
-                CDecl(('std::vector<int>', 'triangle_count_vec', '')),
-                CDecl(('int*', 'count_ptr', '= count.cpu_rd_ptr()')),
-                CBlock(['triangle_count_vec.assign(count_ptr, count_ptr + g.nedges)']),
-                CDecl(('std::vector<int>', 'expected_triangle_count', '= %s' % expected_triangle_count)),
-                CBlock(['EXPECT_EQ(expected_triangle_count, triangle_count_vec)']),
+                CBlock('degree_filter(g, gg)'),
+                CBlock(['EXPECT_EQ(1, 1)']),
                 ]),
             kernel_sizing(),
             main(graph_input),
@@ -68,77 +63,6 @@ class KTrussTests(pyirgltest.test.IrGLTest):
         #                       dst: 1 2 2 0 0 1
         expected_triangle_count = '{ 1,1,1,1,1,1 }'
         self.count_triangle_edges_runner(graph_input, expected_triangle_count)
-
-    @unittest.skipIf(skip_tests, 'count_triangle_edges1')
-    def test_count_triangle_edges1(self):
-        graph_input = '{ { 0,1 }, { 0,2 }, { 0,5 }, { 1,0 }, { 1,2 }, { 2,0 }, { 2,1 }, { 2,3 }, { 2,4 }, { 2,5 }, { 3,2 }, { 4,2 }, { 5,0 }, { 5,2 } }'
-        #                       src: 0 0 0 1 1 2 2 2 2 2 3 4 5 5
-        #                       dst: 2 1 5 0 2 0 1 3 4 5 2 2 0 2
-        #                    valid?: x     x x           x x x x
-        expected_triangle_count = '{ 2,1,1,1,1,2,1,0,0,1,0,0,1,1 }'
-        self.count_triangle_edges_runner(graph_input, expected_triangle_count)
-
-    @unittest.skipIf(skip_tests, 'count_triangle_edges2')
-    def test_count_triangle_edges2(self):
-        graph_input = '{{ 0,1 }, { 0,2 }, { 1,2 }, { 2,3 }, { 3,4 }, { 5,6 }, { 2,5 }, { 2,7 }, { 5,7 }, { 7,8 }, { 7,9 }, { 8,9 }, { 9,10 }, { 10,11 }, { 10,12 }, { 10,13 }, { 11,12 }, { 11,13 }, { 12,13 }, { 13,14 }, { 14,15 },}'
-        #                      src: 0 0 1 1 2 2 2 2 2 3 3 4 5 5 5 6 7 7 7 7 8 8 9 9 9 A A A A B B B C C C D D D D E E F
-        #                      dst: 1 2 2 0 0 1 3 5 7 2 4 3 2 7 6 5 2 5 8 9 7 9 7 A 8 D 9 B C A C D A D B A B C E C F E
-        #                   valid?: x x x             x   x x x   x x       x x x x   x       x x x x x           x   x
-        expected_triangle_count = '{1,1,1,1,1,1,0,1,1,0,0,0,1,1,0,0,1,1,1,1,1,1,1,0,1,2,0,2,2,2,2,2,2,2,2,2,2,2,0,0,0,0 }'
-        self.count_triangle_edges_runner(graph_input, expected_triangle_count)
-
-    def max_ktruss_test_runner(self, graph_input, expected_max_ktruss_size, expected_max_ktruss_nodes):
-        graph = gg.lib.graph.Graph("graph")
-
-        test_ast = Module([
-            CBlock([cgen.Include('edgelist_graph.h')]),
-            CBlock([cgen.Include('gtest/gtest.h')]),
-            Kernel("gg_main", [params.GraphParam('g', True), params.GraphParam('gg', True)], [
-                CBlock(['mgc = mgpu::CreateCudaDevice(CUDA_DEVICE)'], parse=False),
-                CDecl(('Shared<int>', 'count', '(g.nedges)')),
-                CBlock(['count_triangle_edges(g, gg, count)']),
-                CDecl(('int', 'max_ktruss_size', '')),
-                CDecl(('AppendOnlyList', 'max_ktruss_nodes', '(g.nnodes)')),
-                CBlock(['maximal_ktruss(g, gg, count, max_ktruss_size, max_ktruss_nodes)']),
-                CBlock(['EXPECT_EQ(%s, max_ktruss_size)' % expected_max_ktruss_size]),
-                CDecl(('std::vector<int>', 'ktruss_nodes_vec', '(max_ktruss_nodes.nitems())')),
-                CBlock(['ktruss_nodes_vec.assign(max_ktruss_nodes.list.cpu_rd_ptr(), max_ktruss_nodes.list.cpu_rd_ptr()+max_ktruss_nodes.nitems())']),
-                CDecl(('std::vector<int>', 'expected_ktruss_nodes', '= %s' % expected_max_ktruss_nodes)),
-                CBlock(['EXPECT_EQ(expected_ktruss_nodes, ktruss_nodes_vec)']),
-                ]),
-            kernel_sizing(),
-            main(graph_input),
-            ])
-        ast = ktruss.ktruss.ast
-        self.run_test(ast, test_ast)
-
-    @unittest.skipIf(skip_tests, 'max_ktruss0')
-    def test_max_ktruss0(self):
-        graph = '{{ 0, 1 }, { 0, 2 }, { 1, 2 }}'
-        expected_max_ktruss_size = '3'
-        expected_max_ktruss_nodes = '{ 0,1,2 }'
-        self.max_ktruss_test_runner(graph, expected_max_ktruss_size, expected_max_ktruss_nodes)
-
-    @unittest.skipIf(skip_tests, 'max_ktruss1')
-    def test_max_ktruss1(self):
-        graph = '{ { 0,1 }, { 0,2 }, { 0,5 }, { 1,0 }, { 1,2 }, { 2,0 }, { 2,1 }, { 2,3 }, { 2,4 }, { 2,5 }, { 3,2 }, { 4,2 }, { 5,0 }, { 5,2 } }'
-        expected_max_ktruss_size = '4'
-        expected_max_ktruss_nodes = '{ 0,1,2,5 }'
-        self.max_ktruss_test_runner(graph, expected_max_ktruss_size, expected_max_ktruss_nodes)
-
-    @unittest.skipIf(skip_tests, 'max_ktruss2')
-    def test_max_ktruss2(self):
-        graph = '{{ 0,1 }, { 0,2 }, { 1,2 }, { 2,3 }, { 3,4 }, { 5,6 }, { 2,5 }, { 2,7 }, { 5,7 }, { 7,8 }, { 7,9 }, { 8,9 }, { 9,10 }, { 10,11 }, { 10,12 }, { 10,13 }, { 11,12 }, { 11,13 }, { 12,13 }, { 13,14 }, { 14,15 },}'
-        expected_max_ktruss_size = '7'
-        expected_max_ktruss_nodes = '{ 0,1,2,5,7,8,9 }'
-        self.max_ktruss_test_runner(graph, expected_max_ktruss_size, expected_max_ktruss_nodes)
-
-    @unittest.skipIf(skip_tests, 'max_ktruss3')
-    def test_max_ktruss3(self):
-        graph = '{ { 0,1 }, { 0,2 }, { 0,3 }, { 0,4 }, { 1,0 }, { 1,2 }, { 1,3 }, { 1,4 }, { 2,0 }, { 2,1 }, { 2,3 }, { 2,4 }, { 3,0 }, { 3,1 }, { 3,2 }, { 3,4 }, { 4,0 }, { 4,1 }, { 4,2 }, { 4,3 }, }'
-        expected_max_ktruss_size = '5'
-        expected_max_ktruss_nodes = '{ 0,1,2,3,4 }'
-        self.max_ktruss_test_runner(graph, expected_max_ktruss_size, expected_max_ktruss_nodes)
 
 if __name__ == '__main__':
     unittest.main()
