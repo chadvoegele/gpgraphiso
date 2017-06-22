fbec3ed3ca533f7dee737a953e65afbe61401c83
Sreepathi Pai <sreepai@ices.utexas.edu>
2017-06-22 14:04:54 -0500

    Check nopower vs power: statistically identical (ci=95), but system-profiling leads to significant increase in s.d., also add rate per energy to gcmetrics.py

--- eval/scripts/gcmetrics.py
+++ eval/scripts/gcmetrics.py
@@ -2,31 +2,38 @@
 # 
 # Graph challenge metrics
 
-# Total number of edges in the given graph: This measures the amount of data processed
-# Execution time: Total time required to count the triangles or compute the k-truss of the given graph. Time required for reading graph data from a file is not included in this time
-# Rate: Measures the throughput of the implementation as the ratio of the number of edges in the graph to the execution time
-# Energy: Total amount of energy consumption in watts for the computation
-# Rate per energy (edges/second/Watt): Measures the throughput achieved per unit of energy consumed
+#* Total number of edges in the given graph: This measures the amount of data processed
+#* Execution time: Total time required to count the triangles or compute the k-truss of the given graph. Time required for reading graph data from a file is not included in this time
+# TODO: execution time (transferring graph?)
+#* Rate: Measures the throughput of the implementation as the ratio of the number of edges in the graph to the execution time
+#* Energy: Total amount of energy consumption in watts for the computation
+#* Rate per energy (edges/second/Watt): Measures the throughput achieved per unit of energy consumed
 # Memory: Specifies the amount of memory required for the computation
-# Processor: Number and type of processors used in the computation
+#    TODO: compute memory
+#* Processor: Number and type of processors used in the computation
 
 import argparse
 import pandas as pd
 
 p = argparse.ArgumentParser(description="Compute Graph Challenge metrics")
 p.add_argument("perfcsv", help="Performance CSV (from bmk2)")
+p.add_argument("powercsv", help="Power CSV")
 p.add_argument("inputdescsv", help="Input description CSV")
-#p.add_argument("powercsv"
+
 p.add_argument("-o", dest="output", help="Output file")
 
 args = p.parse_args()
 
 perf = pd.read_csv(args.perfcsv)
+powe = pd.read_csv(args.powercsv)
+
 inpd = pd.read_csv(args.inputdescsv)
 
 perf = perf.merge(inpd, 'left', on='input')
+perf = perf.merge(powe[["bmk", "variant", "input", "energy_joules_2_avg", "energy_joules_2_sd", "energy_joules_2_count"]], 'left', on=['bmk', 'variant', 'input'])
 
 perf['rate_eps'] = perf['edges'] * 1E9 / perf['time_ns_avg']
+perf['rate_per_energy'] = perf['rate_eps'] / perf['energy_joules_2_avg']
 
 if args.output:
     perf.to_csv(args.output, index=False)
