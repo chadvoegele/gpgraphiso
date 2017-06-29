f9a4b9a99e5f7e38e9696afd262147e3b073adc0
Sreepathi Pai <sreepai@ices.utexas.edu>
2017-06-29 10:37:44 -0500

    Add max-degree

--- eval/scripts/desc_inputs.py
+++ eval/scripts/desc_inputs.py
@@ -4,6 +4,7 @@ from __future__ import division
 import argparse
 import grf
 import pandas as pd
+import os
 
 def read_inputs(inputs):
     out = {}
@@ -15,11 +16,13 @@ def read_inputs(inputs):
 
     return out
 
-def describe_input(inp, p):
+def describe_input(inp, p, md = {}):
     out = {}
 
     out['input'] = inp
     out['file'] = p
+
+    fb = os.path.basename(p)
 
     g = grf.GRGraphReader()
     hdr = g.read_hdr(out['file'])
@@ -32,19 +35,42 @@ def describe_input(inp, p):
         out['edges'] = out['phy_edges'] // 2
     else:
         out['edges'] = out['phy_edges']
+
+    if fb in md:
+        out['max_degree'] = md[fb]
+    else:
+        out['max_degree'] = None
         
+    return out
+
+def load_max_degree(md):
+    x = read_inputs([md])
+    out = {}
+    for f, d in x.items():
+        fb = os.path.basename(f)
+
+        assert fb not in out, "Duplicate filename %s" % (fb,)
+        
+        out[fb] = d
+
     return out
 
 p = argparse.ArgumentParser(description="Describe input graphs")
 p.add_argument("inputs", nargs="+", help="Files containing inputs and paths")
+p.add_argument("-m", dest="max_degree", help="File containing max degrees")
 
 args = p.parse_args()
 
 all_inputs = read_inputs(args.inputs)
 
+md = {}
+if args.max_degree:
+    print "loading", args.max_degree
+    md = load_max_degree(args.max_degree)
+
 out = []
 for i, p in all_inputs.items():
-    out.append(describe_input(i, p))
+    out.append(describe_input(i, p, md))
 
 df = pd.DataFrame(out)
 df = df.set_index(['input'])
